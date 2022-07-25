8f67ff45d93abd39e9a6b01ec25da3d0e968684e
Kevin Miguta <kevin.miguta@inteligeninfosys.com>
2022-07-25 19:48:27 +0300

    minio port

--- index.js
+++ index.js
@@ -14,10 +14,13 @@ const corsConfig = {
 app.use(cors(corsConfig));
 app.use(BodyParser.json({ limit: "5mb" }));
 
+console.log('MINIO_ENDPOINT==' + process.env.MINIO_ENDPOINT)
+console.log('MINIO_PORT=='+process.env.MINIO_PORT)
+
 var minioClient = new Minio.Client({
     endPoint: process.env.MINIO_ENDPOINT || '127.0.0.1',
     //port: process.env.MINIO_PORT ? parseInt(process.env.MINIO_PORT, 10) : 9005,
-    port: process.env.MINIO_PORT || 9005,
+    port: process.env.MINIO_PORT || parseInt("9005"),
     useSSL: false, 
     accessKey: process.env.ACCESSKEY || 'AKIAIOSFODNN7EXAMPLE',
     secretKey: process.env.SECRETKEY || 'wJalrXUtnFEMIK7MDENGbPxRfiCYEXAMPLEKEY'
